300086272bb8c818ebe1bf52890071a5bba55c11
souhailsegni <contact.souhail.segni@gmail.com>
2025-01-20 17:17:51 +0100

    modifying the terraform-pipline

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "ADD_YOUR_BUCKET_NAME_HERE"             # the bucket
-    region = "ADD_YOUR_REGION_NAME_HERE"             # the region
+    bucket = "ci-cd-terraform-pip"             # the bucket
+    region = "eu-west-1"             # the region
   }
 }
